83215652c6fa0a71f036de62cea3eda31713e230
Mario Lazzari <mario.lazzari@gmail.com>
2024-07-26 23:00:20 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import path from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve("src", "src/components/index.jsx"),
+      name: "react-cv",
+      fileName: format => `react-cv.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [react()],
+});
